9009061f4f10822b5e36fd707912d65d2c78e291
Jeff Hammond <jeff.science@gmail.com>
2014-08-29 12:27:34 -0700

    PSCW test working

--- mpi/rma/shared-memory-windows/win_pscw.c
+++ mpi/rma/shared-memory-windows/win_pscw.c
@@ -1,92 +1,72 @@
 #include <stdio.h>
 #include <mpi.h>
 
-/* This function synchronizes process i with process j
- * in such a way that this function returns on process j
- * only after it has been called on process i.
- *
- * No additional semantic guarantees are provided.
- *
- * The process ranks are with respect to the input communication. */
-
-int p2p_xsync(int i, int j, MPI_Comm comm)
-{
-    /* Avoid deadlock. */
-    if (i==j) {
-        return MPI_SUCCESS;
-    }
-
-    int rank;
-    MPI_Comm_rank(comm, &rank);
-
-    int tag = 666; /* The number of the beast. */
-
-    if (rank==i) {
-        MPI_Send(NULL, 0, MPI_INT, j, tag, comm);
-    } else if (rank==j) {
-        MPI_Recv(NULL, 0, MPI_INT, i, tag, comm, MPI_STATUS_IGNORE);
-    }
-
-    return MPI_SUCCESS;
-}
-
-/* If val is the same at all MPI processes in comm,
- * this function returns 1, else 0. */
-
-int coll_check_equal(int val, MPI_Comm comm)
-{
-    int min, max;
-    MPI_Allreduce(&val, &min, 1, MPI_INT, MPI_MIN, comm);
-    MPI_Allreduce(&val, &max, 1, MPI_INT, MPI_MAX, comm);
-    return (min==max ? 1 : 0);
-}
-
 int main(int argc, char * argv[])
 {
     MPI_Init(&argc, &argv);
 
+    MPI_Group MPI_GROUP_WORLD;
+    MPI_Comm_group(MPI_COMM_WORLD, &MPI_GROUP_WORLD);
+
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (size<2) {
+        printf("You must use at least 2 processes for this test.\n");
+        MPI_Group_free(&MPI_GROUP_WORLD);
+    }
+
+    int ranks[1] = {1};
+    MPI_Group MPI_GROUP_ONE;
+    MPI_Group_incl(MPI_GROUP_WORLD, 1, ranks, &MPI_GROUP_ONE);
+    ranks[0] = 0;
+    MPI_Group MPI_GROUP_ZERO;
+    MPI_Group_incl(MPI_GROUP_WORLD, 1, ranks, &MPI_GROUP_ZERO);
+
 
     int *   shptr = NULL;
     MPI_Win shwin;
     MPI_Win_allocate_shared(rank==0 ? sizeof(int) : 0,sizeof(int),
                             MPI_INFO_NULL, MPI_COMM_WORLD,
                             &shptr, &shwin);
-    MPI_Win_lock_all(0 /* assertion */, shwin);
+
+    MPI_Aint rsize = 0;
+    int rdisp = 0;
+    int * rptr = NULL;
+    MPI_Win_shared_query(shwin, 0, &rsize, &rdisp, &rptr);
 
     if (rank==0) {
+        MPI_Win_post(MPI_GROUP_ONE, 0, shwin);
         *shptr = 42; /* Answer to the Ultimate Question of Life, The Universe, and Everything. */
-        MPI_Win_sync(shwin);
-    }
-    for (int j=1; j<size; j++) {
-        p2p_xsync(0, j, MPI_COMM_WORLD);
-    }
+        MPI_Win_wait(shwin);
 
-    /* l=local r=remote */
-    MPI_Aint rsize = 0;
-    int rdisp;
-    int * rptr = NULL;
-    int lint = -999;
-    MPI_Win_shared_query(shwin, 0, &rsize, &rdisp, &rptr);
-    if (rptr!=NULL && rsize>0) {
-        if (rank!=0) {
-            MPI_Win_sync(shwin);
+        MPI_Win_post(MPI_GROUP_ONE, 0, shwin);
+        MPI_Win_wait(shwin);
+    } else if (rank==1) {
+        int lint = -1;
+        MPI_Win_start(MPI_GROUP_ZERO, 0, shwin);
+        MPI_Win_complete(shwin);
+
+        MPI_Win_start(MPI_GROUP_ZERO, 0, shwin);
+        if (rptr!=NULL && rsize>0) {
+            lint = *rptr;
+        } else {
+            printf("rptr=%p rsize=%zu \n", rptr, (size_t)rsize);
         }
-        lint = *rptr;
-    }
-    if (1==coll_check_equal(lint,MPI_COMM_WORLD)) {
-        if (rank==0) {
+        MPI_Win_complete(shwin);
+        if (lint==42) {
             printf("SUCCESS!\n");
+        } else {
+            printf("lint=%d\n", lint);
         }
-    } else {
-        printf("rank %d: lint = %d \n", rank, lint);
     }
 
-    MPI_Win_unlock_all(shwin);
     MPI_Win_free(&shwin);
 
+    MPI_Group_free(&MPI_GROUP_ONE);
+    MPI_Group_free(&MPI_GROUP_ZERO);
+    MPI_Group_free(&MPI_GROUP_WORLD);
     MPI_Finalize();
 
     return 0;
