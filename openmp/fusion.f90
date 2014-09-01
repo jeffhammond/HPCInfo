2c5341420cb026af297cf11b6cea050b28c3fd51
Jeff Hammond <jeff.science@gmail.com>
2014-08-31 17:17:27 -0700

    add a stupid test for stackoverflow

---
+++ openmp/fusion.f90
@@ -0,0 +1,21 @@
+program main
+    implicit none
+    integer i
+    !$omp parallel
+    !$omp do   
+    do i = 1, 1000
+        print*,'loop 1 ',i
+    enddo
+    !$omp do   
+    do i = 1, 1000
+        print*,'loop 2 ',i
+    enddo
+    !$omp sections
+    !$omp section
+    print*,'section 1'
+    !$omp section
+    print*,'section 2'
+    !$omp end sections
+    !$omp end parallel
+    return
+end program main
