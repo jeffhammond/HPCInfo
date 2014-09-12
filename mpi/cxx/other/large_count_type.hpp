a7d46d12cbc8bd27b83c8265ac68b98fbbd064d4
Jeff Hammond <jeff.science@gmail.com>
2014-09-12 12:59:07 -0700

    test is working

--- mpi/cxx/other/large_count_type.hpp
+++ mpi/cxx/other/large_count_type.hpp
@@ -10,56 +10,66 @@
 #include <iostream>
 #include <mpi.h>
 
-inline void
-SafeMpi( int mpiError )
-{
-    if( mpiError != MPI_SUCCESS )
-    {
-        char errorString[MPI_MAX_ERROR_STRING];
-        int lengthOfErrorString;
-        MPI_Error_string( mpiError, errorString, &lengthOfErrorString );
-        std::cerr << std::string(errorString) << std::endl;
-        MPI_Abort(MPI_COMM_WORLD,1);
-    }
-}
-
 class MpiLargelargeCountType
 {
     public:
         MpiLargelargeCountType(MPI_Count largeCount, MPI_Datatype inType)
         {
-            /* TODO C++-ify this as throw() */
-            /* The largeCount has to fit into MPI_Aint for BigMPI to work. */
+            int mpiOn, mpiOff;
+            MPI_Initialized(&mpiOn);
+            MPI_Finalized(&mpiOff);
+            assert(mpiOn && !mpiOff);
+
             assert(largeCount>=0);
-            assert((unsigned long long)largeCount<(unsigned long long)SIZE_MAX);
 
-            MPI_Count c = largeCount/INT_MAX;
-            MPI_Count r = largeCount%INT_MAX;
+            if ((unsigned long long)largeCount < (unsigned long long)INT_MAX) {
+                MPI_Type_contiguous(static_cast<int>(largeCount), inType, &(this->largeType));
+                MPI_Type_commit(&(this->largeType));
+            } else {
+                /* TODO C++-ify this as throw() */
+                /* The largeCount has to fit into MPI_Aint for BigMPI to work. */
+                assert(static_cast<unsigned long long>(largeCount) < 
+                       static_cast<unsigned long long>(SIZE_MAX));
 
-            MPI_Datatype chunks;
-            MPI_Type_vector(c, INT_MAX, INT_MAX, inType, &chunks);
+                MPI_Count c = largeCount/INT_MAX;
+                MPI_Count r = largeCount%INT_MAX;
 
-            MPI_Datatype remainder;
-            MPI_Type_contiguous(r, inType, &remainder);
+                MPI_Datatype chunks;
+                MPI_Type_vector(c, INT_MAX, INT_MAX, inType, &chunks);
 
-            MPI_Aint lb /* unused */, extent;
-            MPI_Type_get_extent(inType, &lb, &extent);
+                MPI_Datatype remainder;
+                MPI_Type_contiguous(r, inType, &remainder);
 
-            MPI_Aint remdisp          = (MPI_Aint)c*INT_MAX*extent;
-            int blocklengths[2]       = {1,1};
-            MPI_Aint displacements[2] = {0,remdisp};
-            MPI_Datatype types[2]     = {chunks,remainder};
-            MPI_Type_create_struct(2, blocklengths, displacements, types, &(this->largeType));
+                MPI_Aint lb /* unused */, extent;
+                MPI_Type_get_extent(inType, &lb, &extent);
 
-            MPI_Type_free(&chunks);
-            MPI_Type_free(&remainder);
+                MPI_Aint remdisp          = static_cast<MPI_Aint>(c)*static_cast<MPI_Aint>(INT_MAX)*extent;
+                int blocklengths[2]       = {1,1};
+                MPI_Aint displacements[2] = {0,remdisp};
+                MPI_Datatype types[2]     = {chunks,remainder};
+                MPI_Type_create_struct(2, blocklengths, displacements, types, &(this->largeType));
+                MPI_Type_commit(&(this->largeType));
 
-
-        }
+                MPI_Type_free(&chunks);
+                MPI_Type_free(&remainder);
+            } // largeCount < INT_MAX
+        } // ctor
 
         ~MpiLargelargeCountType()
         {
+            /* If user declares this in scope of main(), 
+             * then the destructor will be called after MPI_Finalize() */
+            int mpiOn, mpiOff;
+            MPI_Initialized(&mpiOn);
+            MPI_Finalized(&mpiOff);
+            assert(mpiOn && !mpiOff);
+
             MPI_Type_free(&(this->largeType));
+        }
+
+        MPI_Datatype GetMpiDatatype()
+        {
+            return this->largeType;
         }
 
     private:
--- mpi/cxx/other/test_large_count_type.cpp
+++ mpi/cxx/other/test_large_count_type.cpp
@@ -1,8 +1,43 @@
 #include "large_count_type.hpp"
+#include <cstring> // memset
 
 int main(int argc, char* argv[])
 {
+    MPI_Init(&argc,&argv);
 
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    long n = (argc>1) ? atol(argv[1]) : 1000;
+
+    char * buf = NULL;
+    MPI_Alloc_mem(n, MPI_INFO_NULL, &buf);
+    if (rank==0) {
+        memset(buf, 'Z', n);
+    } else {
+        memset(buf, 'A', n);
+    }
+
+    {
+        MpiLargelargeCountType bigtype(n,MPI_CHAR);
+        MPI_Bcast(buf,
+                  1, bigtype.GetMpiDatatype(),
+                  0, MPI_COMM_WORLD);
+        /* ~MpiLargelargeCountType happens here */
+    }
+
+    void * test = malloc(n);
+    memset(test, 'Z', n);
+    int rc = memcmp(static_cast<void*>(buf), test, n);
+    if (rc!=0) {
+        std::cout << "There were " << n << " error!" << std::endl;
+    }
+
+    free(test);
+    MPI_Free_mem(buf);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
 
     return 0;
 }
