13db8523f9021ed7ddaf69d6b1e5c6bb1dc89497
Jeff Hammond <jeff.science@gmail.com>
2014-09-16 15:01:41 -0700

    thread test works

--- c++11/threads.cxx
+++ c++11/threads.cxx
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <thread>
 
+void fnoarg(int t)
+{
+    std::this_thread::sleep_for( std::chrono::seconds(t) );
+    std::cout << "fnoarg running on thread " << std::this_thread::get_id() << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
-    std::cout << "hw concurrency = " << std::thread::hardware_concurrency() << std::endl;
+    int nthreads = std::thread::hardware_concurrency();
+    std::cout << "nthreads = " << nthreads << std::endl;
+
+    std::thread * pool = new std::thread[nthreads];
+    for (int i=0; i<nthreads; ++i) {
+        pool[i] = std::thread(fnoarg,i);
+    }
+    for (int i=0; i<nthreads; ++i) {
+        pool[i].join();
+    }
+    delete[] pool;
+
     return 0;
 }
