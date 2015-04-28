c33a0fab1f0c18e6ed1242c1e3b152c4852b8770
Jeff Hammond <jeff.science@gmail.com>
2015-04-28 14:59:00 -0700

    add experiments for stack overflow

--- c99/alias2d.c
+++ c99/alias2d.c
@@ -10,13 +10,16 @@ void fn0(const float *restrict a0, const float *restrict a1,
         b[i] = a0[i] + a1[i];
 }
 
-#ifdef OLD
-void fn1(const float *restrict *restrict a, float *restrict b, int n)
-#else
+#if defined(ARRAY_RESTRICT)
 void fn1(const float *restrict a[restrict], float * restrict b, int n)
+#elif defined(TYPEDEF_SOLUTION)
+typedef float * restrict frp;
+void fn1(const frp *restrict a, float *restrict b, int n)
+#else
+void fn1(const float *restrict *restrict a, float *restrict b, int n)
 #endif
 {
-    ASSUME_ALIGNED(a[0]); ASSUME_ALIGNED(a[1]); ASSUME_ALIGNED(b);
+    //ASSUME_ALIGNED(a[0]); ASSUME_ALIGNED(a[1]); ASSUME_ALIGNED(b);
 
     for (int i = 0; i < n; ++i)
         b[i] = a[0][i] + a[1][i];
