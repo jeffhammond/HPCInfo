1c6096c56c90c0e7a91bfe7f52432dd75883dae9
Jeff Hammond <jeff.science@gmail.com>
2015-12-03 16:03:03 -0800

    fix issues with gems code

--- coarray-f/gems.f
+++ coarray-f/gems.f
@@ -1,17 +1,16 @@
-program main
-implicit none
-integer, allocatable :: A(:)[:]
-integer :: B
-if (num_images()<2) call abort;
-! allocate from the shared heap
-allocate(A(1)[*])
-B = 37;
-! store contents of local B at PE 0 into A at PE 1
-if (this_image().eq.0) A[1] = B;
-! global synchronization of execution and data
-sync all
-! observe the result of the store
-if (this_image().eq.1) print*,'A@1=',A(1)[1]
-deallocate(A)
-end program main
-
+      program main
+      implicit none
+      integer, allocatable :: A(:)[:]
+      integer :: B
+      if (num_images()<2) call abort;
+      ! allocate from the shared heap
+      allocate(A(1)[*])
+      B = 37;
+      ! store contents of local B at PE 0 into A at PE 1
+      if (this_image().eq.0) A(1)[1] = B;
+      ! global synchronization of execution and data
+      sync all
+      ! observe the result of the store
+      if (this_image().eq.1) print*,'A@1=',A(1)[1]
+      deallocate(A)
+      end program main
