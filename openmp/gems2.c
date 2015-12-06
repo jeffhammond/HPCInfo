0b430275c2fed7f0af2fa2c6400a5902814f50a0
Jeff Hammond <jeff.science@gmail.com>
2015-12-06 15:27:46 -0800

    better examples

--- openmp/gems2.c
+++ openmp/gems2.c
@@ -6,17 +6,20 @@ int main(void) {
     int np = omp_get_max_threads();
     if (np<2) exit(1);
     /* allocate from the heap with global visibility */
-    int * A = malloc(np*sizeof(int));
+    int ** A = malloc(np*sizeof(int*));
     #pragma omp parallel shared(A)
     {
         int me = omp_get_thread_num();
+        A[me] = malloc(sizeof(int));
+        #pragma omp barrier
         int B = 134;
         /* store local data B at PE 0 into A at PE 1 */
-        if (me==0) A[1] = B;
+        if (me==0) A[1][0] = B;
         /* global synchronization of execution and data */
         #pragma omp barrier
         /* observe the result of the store */
-        if (me==1) printf("A@1=%d\n",A[1]); fflush(stdout);
+        if (me==1) printf("A@1=%d\n",A[1][0]); fflush(stdout);
+        free(A[me]);
     }
     free(A);
     return 0;
