85416ddc27da9a83bb0256577c0b8008ea764ed0
Jeff Hammond <jeff.science@gmail.com>
2015-12-21 10:00:22 -0800

    TBB hello world - printout is not atomic

--- tbb/hello.cxx
+++ tbb/hello.cxx
@@ -1,6 +1,14 @@
+#include <iostream>
 #include "tbb/tbb.h"
 
 int main(int argc, char* argv[])
 {
+    int n = tbb::task_scheduler_init::default_num_threads();
+    tbb::parallel_for(size_t(0), size_t(n), size_t(1),
+            [=](size_t i)
+            {
+                std::cout << i;
+            });
+    std::cout << std::endl;
     return 0;
 }
