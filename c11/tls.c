7d735f382c5427ae6b86b247c350b03535cb8646
Jeff Hammond <jeff.science@gmail.com>
2016-01-08 12:24:05 -0800

    remove OpenMP

--- c11/tls.c
+++ c11/tls.c
@@ -2,19 +2,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <threads.h>
-#include <omp.h>
-
-int p;
-#pragma omp threadprivate(p)
-int s;
+#include <threads.h>
 
 int main(int argc, char **argv)
 {
-    #pragma omp parallel
-    {
-        printf("%d: %p %p\n", omp_get_thread_num(), &p, &s );
-    }
     return 0;
 }
 
