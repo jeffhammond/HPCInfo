78470b9a003157c920c8de145152336fc69f89c1
Jeff Hammond <jeff.science@gmail.com>
2016-01-10 08:36:27 -0800

    remove C11 from TLS OpenMP example

--- openmp/tls.c
+++ openmp/tls.c
@@ -1,8 +1,5 @@
-#if (__STDC_VERSION__ >= 201112L) && !defined(__STDC_NO_THREADS__)
-
 #include <stdio.h>
 #include <stdlib.h>
-//#include <threads.h>
 #include <omp.h>
 
 int p;
@@ -17,5 +14,3 @@ int main(int argc, char **argv)
     }
     return 0;
 }
-
-#endif
