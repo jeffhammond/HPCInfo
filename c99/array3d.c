8d4e27644d0a9d39ee0d00e84fe6120f2b819cf0
Jeff Hammond <jeff.science@gmail.com>
2016-02-12 14:56:00 -0800

    add 3D example for Ondřej Čertík

--- c99/array3d.c
+++ c99/array3d.c
@@ -1,46 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Both of these signatures are acceptable.
- * The first one looks more like the cast below.
- * The second format does not work as a cast below. */
-#if 1
-void print2(int m, int n, double (* const restrict A)[n])
-#else
-void print2(int m, int n, double (A[restrict])[n])
-#endif
+void print3(int m, int n, int r, double (* const restrict A)[n][r])
 {
     for(int i=0; i<m; i++)
         for(int j=0; j<n; j++)
-            printf("(%d,%d)=%lf\n",i,j,A[i][j]);
-
+            for(int k=0; k<r; k++)
+                printf("(%d,%d,%d)=%lf\n",i,j,k,A[i][j][k]);
 }
 
-void print1(int m, int n, double A[])
+void print1(int m, double A[])
 {
-    for(int i=0; i<(m*n); i++)
+    for(int i=0; i<m; i++)
         printf("(%d)=%lf\n",i,A[i]);
 }
 
 int main(int argc, char* argv[])
 {
-    int m = (argc>1) ? atoi(argv[1]) : 10;
-    int n = (argc>2) ? atoi(argv[2]) : 5;
+    int m = (argc>1) ? atoi(argv[1]) : 3;
+    int n = (argc>2) ? atoi(argv[2]) : 4;
+    int r = (argc>3) ? atoi(argv[3]) : 5;
 
-    double * restrict A = (double*)malloc(m*n*sizeof(double));
-    for(int i=0; i<(m*n); i++)
+    double * restrict A = (double*)malloc(m*n*r*sizeof(double));
+    for(int i=0; i<(m*n*r); i++)
         A[i] = (double)i;
 
-    double (* const restrict B)[n] = (double (*)[n]) malloc(m*n*sizeof(double));
+    double (* const restrict B)[n][r] = (double (*)[n][r]) malloc(m*n*r*sizeof(double));
     for(int i=0; i<m; i++)
         for(int j=0; j<n; j++)
-            B[i][j] = (double)(i*n+j);
+            for(int k=0; k<r; k++)
+                B[i][j][k] = (double)(i*n*r+j*r+k);
 
-    print1(m,n,A);
-    print2(m,n,(double (*)[n])A);
+    print1(m*n*r,A);
+    print3(m,n,r,(double (*)[n][r])A);
 
-    print1(m,n,(double * restrict)B);
-    print2(m,n,B);
+    print1(m*n*r,(double *)B);
+    print3(m,n,r,B);
 
     free(B);
     free(A);
