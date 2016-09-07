579a856685da230d4eb824fcc001bc227f6f0c6b
Jeff Hammond <jeff.science@gmail.com>
2016-09-07 12:51:22 -0700

    add sizeof

--- atomics/ping-pong/c11-insane.c
+++ atomics/ping-pong/c11-insane.c
@@ -2,16 +2,14 @@
 #include <complex.h>
 #include <stdatomic.h>
 
-_Atomic long double _Complex x;
 
 int main(void)
 {
-    x = 0.0 + 0.0*I;
+    _Atomic long double _Complex x = 0.0 + 0.0*I;
     for (int i=0; i<1000; i++) {
         x += 1.0;
     }
-
     printf("(%Lf,%Lf)\n",creall(x), cimagl(x));
-
+    printf("%zu\n",sizeof(x));
     return 0;
 }
