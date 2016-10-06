12f9a3feb9675d0c544f0a6295d6f048dffe20d7
Jeff Hammond <jeff.science@gmail.com>
2016-10-06 08:52:07 -0700

    add memory model support and _Pragma stuff

--- atomics/summation/cxx11-counter.cc
+++ atomics/summation/cxx11-counter.cc
@@ -7,16 +7,26 @@
 
 #include <chrono>
 
-#ifdef _OPENMP
-# include <omp.h>
-#else
-# error No OpenMP support!
-#endif
-
 #ifdef SEQUENTIAL_CONSISTENCY
 auto update_model = std::memory_order_seq_cst;
 #else
 auto update_model = std::memory_order_relaxed;
+#endif
+
+#ifdef _OPENMP
+# include <omp.h>
+#define OMP_PARALLEL            _Pragma("omp parallel")
+#define OMP_BARRIER             _Pragma("omp barrier")
+#define OMP_CRITICAL            _Pragma("omp critical")
+#ifdef SEQUENTIAL_CONSISTENCY
+#define OMP_ATOMIC              _Pragma("omp atomic seq_cst")
+#define OMP_ATOMIC_CAPTURE      _Pragma("omp atomic capture seq_cst")
+#else
+#define OMP_ATOMIC              _Pragma("omp atomic")
+#define OMP_ATOMIC_CAPTURE      _Pragma("omp atomic capture")
+#endif
+#else
+# error No OpenMP support!
 #endif
 
 int main(int argc, char * argv[])
@@ -36,13 +46,14 @@ int main(int argc, char * argv[])
     std::cout << "1) std::atomic_fetch_add(&counter, 1)\n";
 
     std::atomic<int> counter = {0};
+    int omp_counter = 0;
 
-    #pragma omp parallel
+    OMP_PARALLEL
     {
         std::atomic_thread_fence(std::memory_order_seq_cst);
 
         /// START TIME
-        #pragma omp barrier
+        OMP_BARRIER
         std::chrono::high_resolution_clock::time_point t0 = std::chrono::high_resolution_clock::now();
 
         for (int i=0; i<iterations; ++i) {
@@ -50,14 +61,14 @@ int main(int argc, char * argv[])
         }
 
         /// STOP TIME
-        #pragma omp barrier
+        OMP_BARRIER
         std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
         std::atomic_thread_fence(std::memory_order_seq_cst);
 
         /// PRINT TIME
         std::chrono::duration<double> dt = std::chrono::duration_cast<std::chrono::duration<double>>(t1-t0);
-        #pragma omp critical
+        OMP_CRITICAL
         {
             std::cout << "total time elapsed = " << dt.count() << "\n";
             std::cout << "time per iteration = " << dt.count()/iterations  << "\n";
@@ -69,12 +80,12 @@ int main(int argc, char * argv[])
 
     counter = 0;
 
-    #pragma omp parallel
+    OMP_PARALLEL
     {
         std::atomic_thread_fence(std::memory_order_seq_cst);
 
         /// START TIME
-        #pragma omp barrier
+        OMP_BARRIER
         std::chrono::high_resolution_clock::time_point t0 = std::chrono::high_resolution_clock::now();
 
         for (int i=0; i<iterations; ++i) {
@@ -82,14 +93,14 @@ int main(int argc, char * argv[])
         }
 
         /// STOP TIME
-        #pragma omp barrier
+        OMP_BARRIER
         std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
         std::atomic_thread_fence(std::memory_order_seq_cst);
 
         /// PRINT TIME
         std::chrono::duration<double> dt = std::chrono::duration_cast<std::chrono::duration<double>>(t1-t0);
-        #pragma omp critical
+        OMP_CRITICAL
         {
             std::cout << "total time elapsed = " << dt.count() << "\n";
             std::cout << "time per iteration = " << dt.count()/iterations  << "\n";
@@ -101,12 +112,12 @@ int main(int argc, char * argv[])
 
     counter = 0;
 
-    #pragma omp parallel
+    OMP_PARALLEL
     {
         std::atomic_thread_fence(std::memory_order_seq_cst);
 
         /// START TIME
-        #pragma omp barrier
+        OMP_BARRIER
         std::chrono::high_resolution_clock::time_point t0 = std::chrono::high_resolution_clock::now();
 
         for (int i=0; i<iterations; ++i) {
@@ -114,14 +125,14 @@ int main(int argc, char * argv[])
         }
 
         /// STOP TIME
-        #pragma omp barrier
+        OMP_BARRIER
         std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
         std::atomic_thread_fence(std::memory_order_seq_cst);
 
         /// PRINT TIME
         std::chrono::duration<double> dt = std::chrono::duration_cast<std::chrono::duration<double>>(t1-t0);
-        #pragma omp critical
+        OMP_CRITICAL
         {
             std::cout << "total time elapsed = " << dt.count() << "\n";
             std::cout << "time per iteration = " << dt.count()/iterations  << "\n";
@@ -133,14 +144,14 @@ int main(int argc, char * argv[])
 
     counter = 0;
 
-    #pragma omp parallel
+    OMP_PARALLEL
     {
         int output = -1;
 
         std::atomic_thread_fence(std::memory_order_seq_cst);
 
         /// START TIME
-        #pragma omp barrier
+        OMP_BARRIER
         std::chrono::high_resolution_clock::time_point t0 = std::chrono::high_resolution_clock::now();
 
         for (int i=0; i<iterations; ++i) {
@@ -148,14 +159,14 @@ int main(int argc, char * argv[])
         }
 
         /// STOP TIME
-        #pragma omp barrier
+        OMP_BARRIER
         std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
         std::atomic_thread_fence(std::memory_order_seq_cst);
 
         /// PRINT TIME
         std::chrono::duration<double> dt = std::chrono::duration_cast<std::chrono::duration<double>>(t1-t0);
-        #pragma omp critical
+        OMP_CRITICAL
         {
             std::cout << "total time elapsed = " << dt.count() << "\n";
             std::cout << "time per iteration = " << dt.count()/iterations  << "\n";
@@ -168,14 +179,14 @@ int main(int argc, char * argv[])
 
     counter = 0;
 
-    #pragma omp parallel
+    OMP_PARALLEL
     {
         int output = -1;
 
         std::atomic_thread_fence(std::memory_order_seq_cst);
 
         /// START TIME
-        #pragma omp barrier
+        OMP_BARRIER
         std::chrono::high_resolution_clock::time_point t0 = std::chrono::high_resolution_clock::now();
 
         for (int i=0; i<iterations; ++i) {
@@ -183,14 +194,75 @@ int main(int argc, char * argv[])
         }
 
         /// STOP TIME
-        #pragma omp barrier
+        OMP_BARRIER
         std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
         std::atomic_thread_fence(std::memory_order_seq_cst);
 
         /// PRINT TIME
         std::chrono::duration<double> dt = std::chrono::duration_cast<std::chrono::duration<double>>(t1-t0);
-        #pragma omp critical
+        OMP_CRITICAL
+        {
+            std::cout << "total time elapsed = " << dt.count() << "\n";
+            std::cout << "time per iteration = " << dt.count()/iterations  << "\n";
+            std::cout << counter << std::endl;
+            std::cout << output << std::endl;
+        }
+    }
+
+    std::cout << "6) #pragma omp atomic\n";
+
+    omp_counter = 0;
+
+    OMP_PARALLEL
+    {
+        /// START TIME
+        OMP_BARRIER
+        std::chrono::high_resolution_clock::time_point t0 = std::chrono::high_resolution_clock::now();
+
+        for (int i=0; i<iterations; ++i) {
+            OMP_ATOMIC
+            omp_counter++;
+        }
+
+        /// STOP TIME
+        OMP_BARRIER
+        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+
+        /// PRINT TIME
+        std::chrono::duration<double> dt = std::chrono::duration_cast<std::chrono::duration<double>>(t1-t0);
+        OMP_CRITICAL
+        {
+            std::cout << "total time elapsed = " << dt.count() << "\n";
+            std::cout << "time per iteration = " << dt.count()/iterations  << "\n";
+            std::cout << counter << std::endl;
+        }
+    }
+
+    std::cout << "7) #pragma omp atomic capture\n";
+
+    counter = 0;
+
+    OMP_PARALLEL
+    {
+        int output = -1;
+
+        /// START TIME
+        OMP_BARRIER
+        std::chrono::high_resolution_clock::time_point t0 = std::chrono::high_resolution_clock::now();
+
+        for (int i=0; i<iterations; ++i) {
+            OMP_ATOMIC_CAPTURE
+            output = omp_counter++;
+        }
+
+        /// STOP TIME
+        OMP_BARRIER
+        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+
+        /// PRINT TIME
+        std::chrono::duration<double> dt = std::chrono::duration_cast<std::chrono::duration<double>>(t1-t0);
+        OMP_CRITICAL
         {
             std::cout << "total time elapsed = " << dt.count() << "\n";
             std::cout << "time per iteration = " << dt.count()/iterations  << "\n";
