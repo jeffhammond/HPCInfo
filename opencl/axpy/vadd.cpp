9b564a7d91f5ab67dd95eff4af25afe2a529b0a8
Jeff Hammond <jeff.science@gmail.com>
2016-10-07 05:44:02 -0700

    cleanup code

--- opencl/axpy/vadd.cpp
+++ opencl/axpy/vadd.cpp
@@ -20,9 +20,9 @@
 #include "util.hpp" // utility library
 
 #include <vector>
-#include <cstdio>
-#include <cstdlib>
-#include <string>
+//#include <cstdio>
+//#include <cstdlib>
+//#include <string>
 
 #include <iostream>
 #include <fstream>
@@ -36,31 +36,28 @@
 
 //------------------------------------------------------------------------------
 
-#define TOL    (0.001)   // tolerance used in floating point comparisons
-//#define LENGTH (1024)    // length of vectors a, b, and c
+const double threshold = 0.001;
 
 int main(int argc, char* argv[])
 {
-    const long LENGTH = (argc>1) ? atol(argv[1]) : 1024;
+    const int count = (argc>1) ? atoi(argv[1]) : 1024;
 
-    std::vector<float> h_a(LENGTH);              // a vector 
-    std::vector<float> h_b(LENGTH);              // b vector 	
-    std::vector<float> h_c (LENGTH, 0xdeadbeef); // c = a + b, from compute device
+    std::vector<float> h_a(count);             // a vector
+    std::vector<float> h_b(count);             // b vector
+    std::vector<float> h_c(count, 0xdeadbeef); // c = a + b, from compute device
 
     cl::Buffer d_a;      // device memory used for the input  a vector
     cl::Buffer d_b;      // device memory used for the input  b vector
     cl::Buffer d_c;      // device memory used for the output c vector
 
     // Fill vectors a and b with random float values
-    int count = LENGTH;
-    for(int i = 0; i < count; i++)
-    {
+    // FIXME: replace with C++11 RNG
+    for(int i = 0; i < count; ++i) {
         h_a[i]  = rand() / (float)RAND_MAX;
         h_b[i]  = rand() / (float)RAND_MAX;
     }
 
-    try 
-    {
+    try {
     	// Create a context
         cl::Context context(DEVICE);
 
@@ -72,74 +69,41 @@ int main(int argc, char* argv[])
         cl::CommandQueue queue(context);
 
         // Create the kernel functor
- 
+
         auto vadd = cl::make_kernel<cl::Buffer, cl::Buffer, cl::Buffer, int>(program, "vadd");
 
-        d_a   = cl::Buffer(context, begin(h_a), end(h_a), true);
-        d_b   = cl::Buffer(context, begin(h_b), end(h_b), true);
-
-        d_c  = cl::Buffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * LENGTH);
+        d_a  = cl::Buffer(context, begin(h_a), end(h_a), true);
+        d_b  = cl::Buffer(context, begin(h_b), end(h_b), true);
+        d_c  = cl::Buffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * count);
 
         util::Timer timer;
 
-        vadd(
-            cl::EnqueueArgs(
-                queue,
-                cl::NDRange(count)), 
-            d_a,
-            d_b,
-            d_c,
-            count);
+        vadd(cl::EnqueueArgs( queue, cl::NDRange(count)), d_a, d_b, d_c, count);
 
         queue.finish();
 
         double rtime = static_cast<double>(timer.getTimeMilliseconds()) / 1000.0;
-        printf("\nThe kernels ran in %lf seconds\n", rtime);
+        std::cout << "The kernels ran in " << rtime << " seconds\n";
 
         cl::copy(queue, d_c, begin(h_c), end(h_c));
 
         // Test the results
         int correct = 0;
-        float tmp;
-        for(int i = 0; i < count; i++) {
-            tmp = h_a[i] + h_b[i];    // expected value for d_c[i]
-            tmp -= h_c[i];            // compute errors
-            if(tmp*tmp < TOL*TOL) {   // correct if square deviation is less 
-                correct++;            //  than tolerance squared
-            }
-            else {
-
-                printf(
-                    " tmp %f h_a %f h_b %f  h_c %f \n",
-                    tmp, 
-                    h_a[i], 
-                    h_b[i], 
-                    h_c[i]);
+        for(int i = 0; i < count; ++i) {
+            float tmp = (h_a[i] + h_b[i]) - h_c[i];
+            if(tmp*tmp < threshold*threshold) {
+                correct++;
+            } else {
+                std::cerr << " tmp=" << tmp << " h_a=" << h_a[i] << " h_b=" << h_b[i] << " h_c=" << h_c[i] << " \n";
             }
         }
 
         // summarize results
-        printf(
-            "vector add to find C = A+B:  %d out of %d results were correct.\n", 
-            correct, 
-            count);
+        std::cout << "vector add to find C = A+B: " << correct << " out of " << count << "  results were correct.\n";
     }
     catch (cl::Error err) {
         std::cout << "Exception\n";
-        std::cerr 
-            << "ERROR: "
-            << err.what()
-           << std::endl;
+        std::cerr << "ERROR: " << err.what() << std::endl;
     }
+    return 0;
 }
-//    catch (cl::Error err) {
-//        std::cout << "Exception\n";
-//        std::cerr 
-//            << "ERROR: "
-//            << err.what()
-//            << "("
-//            << err_code(err.err())
-//           << ")"
-//           << std::endl;
-//    }
-//}
