399a4f5583e939ec28f1664a639e31fdb221e6ff
Jeff Hammond <jeff.science@gmail.com>
2016-10-20 21:16:23 -0700

    C99/OpenMP version of Dekker implemented

--- atomics/ping-pong/c99-omp-dekker.c
+++ atomics/ping-pong/c99-omp-dekker.c
@@ -8,101 +8,66 @@
 # error No OpenMP support!
 #endif
 
-#ifdef SEQUENTIAL_CONSISTENCY
-# if ( _OPENMP < 201307 )
-#  error You need OpenMP 4+ for seq_cst atomics.
-# endif
-# define OMP_ATOMIC_LOAD  _Pragma("omp atomic read seq_cst")
-# define OMP_ATOMIC_STORE _Pragma("omp atomic write seq_cst")
-# define OMP_FLUSH
+#if ( _OPENMP < 201307 )
+# error You need OpenMP 4+ for seq_cst atomics.
 #else
-# define OMP_ATOMIC_LOAD  _Pragma("omp atomic read")
-# define OMP_ATOMIC_STORE _Pragma("omp atomic write")
-# define OMP_FLUSH        _Pragma("omp flush")
+# define OMP_ATOMIC_LOAD_SC  _Pragma("omp atomic read seq_cst")
+# define OMP_ATOMIC_STORE_SC _Pragma("omp atomic write seq_cst")
+# define OMP_ATOMIC_LOAD     _Pragma("omp atomic read")
+# define OMP_ATOMIC_STORE    _Pragma("omp atomic write")
 #endif
 
 int main(int argc, char * argv[])
 {
     int nt = omp_get_max_threads();
-#if 1
     if (nt != 2) omp_set_num_threads(2);
-#else
-    if (nt < 2)      omp_set_num_threads(2);
-    if (nt % 2 != 0) omp_set_num_threads(nt-1);
-#endif
 
     int iterations = (argc>1) ? atoi(argv[1]) : 1000000;
 
-    printf("thread ping-pong benchmark\n");
+    printf("Dekker benchmark\n");
     printf("num threads  = %d\n", omp_get_max_threads());
     printf("iterations   = %d\n", iterations);
-#ifdef SEQUENTIAL_CONSISTENCY
-    printf("memory model = %s\n", "seq_cst");
-#else
-    printf("memory model = %s\n", "acq-rel");
-#endif
     fflush(stdout);
 
-    int left_ready  = -1;
-    int right_ready = -1;
-
-    int left_payload  = 0;
-    int right_payload = 0;
+    int x=0, y=0;
+    int lcount=0;
+    int rcount=0;
 
     #pragma omp parallel
     {
-        int me      = omp_get_thread_num();
-        /// 0=left 1=right
-        bool parity = (me % 2 == 0);
-
-        int junk = 0;
+        int me = omp_get_thread_num();
+        int temp = -1;
 
         /// START TIME
         #pragma omp barrier
         double t0 = omp_get_wtime();
 
         for (int i=0; i<iterations; ++i) {
-
-            if (parity) {
-
-                /// send to left
-                left_payload = i;
+            if (me==0) {
                 OMP_ATOMIC_STORE
-                left_ready = i;
-                //OMP_FLUSH
-
-                /// recv from right
-                while (1) {
-                    OMP_FLUSH
-                    int temp;
-                    OMP_ATOMIC_LOAD
-                    temp = i;
-                    if (temp == right_ready) break;
+                x=1;
+                OMP_ATOMIC_LOAD_SC
+                temp=y;
+                if (temp==0) {
+                    lcount++;
+                    temp=0;
                 }
-                //printf("%d: left received %d\n", i, right_payload);
-                junk += right_payload;
-
             } else {
-
-                /// recv from left
-                while (1) {
-                    OMP_FLUSH
-                    int temp;
-                    OMP_ATOMIC_LOAD
-                    temp = i;
-                    if (temp == left_ready) break;
-                }
-                //printf("%d: right received %d\n", i, left_payload);
-                junk += left_payload;
-
-                ///send to right
-                right_payload = i;
                 OMP_ATOMIC_STORE
-                right_ready = i;
-                //OMP_FLUSH
-
+                y=1;
+                OMP_ATOMIC_LOAD_SC
+                temp=x;
+                if (temp==0) {
+                    rcount++;
+                    temp=0;
+                }
             }
-
+            /// reset flags
+            #pragma omp single
+            {
+                x=0;
+                y=0;
+            }
         }
 
         /// STOP TIME
@@ -115,7 +80,7 @@ int main(int argc, char * argv[])
         {
             printf("total time elapsed = %lf\n", dt);
             printf("time per iteration = %e\n", dt/iterations);
-            printf("%d\n", junk);
+            printf("lcount=%d, rcount=%d\n", lcount, rcount);
         }
     }
 
