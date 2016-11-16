cca33a7ecaeb04f1a33d002a297b3e02dbbc344b
Jeff Hammond <jeff.science@gmail.com>
2016-11-16 11:50:15 -0800

    this works

--- linux/vmem/test-mprotect.c
+++ linux/vmem/test-mprotect.c
@@ -3,30 +3,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stddef.h>
 #include <string.h>
 #include <assert.h>
 
+#include <signal.h>
+#include <unistd.h>
 #include <sys/mman.h>
 #include <errno.h>
 
+char * t = "SIGSEGV at address ";
+char * a = "                   ";
+char * m = "                                         ";
+
+ssize_t junk = 0;
+
+size_t pagesize;
+
+static void handler(int sig, siginfo_t * info, void * state)
+{
+#if 0
+    sprintf(a,"%p",info->si_addr);
+    strcat(m,t);
+    strcat(m,a);
+    junk = write(STDOUT_FILENO, m, strlen(m));
+#else
+    printf("handler: SIGSEGV at address %p\n", info->si_addr);
+#endif
+    void * x = info->si_addr;
+    junk = mprotect(x,pagesize,PROT_WRITE);
+    if (junk) abort();
+    printf("handler: mprotect %p:%p PROT_WRITE\n",x,x+pagesize);
+}
+
 int main(int argc, char* argv[])
 {
-    long long n = (argc>1) ? atoll(argv[1]) : 1024*1024*1024;
-    for (size_t i=1; i<=n; i*=2) {
-        size_t bytes = i;
-        printf("bytes=%zu\n",bytes);
-        {
-            int prot  = PROT_READ | PROT_WRITE;      /* read-write memory    */
-            int flags = MAP_ANONYMOUS |              /* no file backing      */ 
-                        MAP_NORESERVE |              /* do not reserve swap  */
-                        MAP_POPULATE |               /* pre-fault pages      */
-                        MAP_PRIVATE;                 /* not shared memory    */
-            char * ptr = mmap(NULL, bytes, prot, flags, -1, 0);
-            printf("ptr=%p\n", ptr);
-            memset(ptr,0,bytes);
-            int rc = munmap(ptr,bytes);
-            printf("rc=%d\n", rc);
-        }
-    }
+    int rc = 0;
+
+    pagesize = sysconf(_SC_PAGESIZE);
+    printf("pagesize = %zu\n", pagesize);
+
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = handler;
+    rc = sigaction(SIGSEGV, &sa, NULL);
+    assert(rc==0);
+    printf("sigaction set for SIGSEGV\n");
+
+    size_t n = pagesize;
+    char * x = NULL;
+    rc = posix_memalign((void**)&x,pagesize,n);
+    assert(x!=NULL && rc==0);
+    memset(x,'f',n);
+    printf("x=%p is allocated and bits are oned\n", x);
+
+    rc = mprotect(x,pagesize,PROT_NONE);
+    assert(rc==0);
+    printf("mprotect %p:%p PROT_NONE\n",x,x+pagesize);
+
+    memset(x,'a',n);
+
+    rc = mprotect(x,pagesize,PROT_READ);
+    assert(rc==0);
+    printf("mprotect %p:%p PROT_READ\n",x,x+pagesize);
+
+    printf("x = %s\n", x);
+
+    free(x);
+    printf("SUCCESS\n");
     return 0;
 }
