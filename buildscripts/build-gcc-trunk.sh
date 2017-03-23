9a18d67cd267178729badd15541a3d586a531fac
Jeff Hammond <jeff.science@gmail.com>
2017-03-23 08:44:31 -0600

    improve gcc git build script

--- buildscripts/build-gcc-trunk.sh
+++ buildscripts/build-gcc-trunk.sh
@@ -1,14 +1,27 @@
 #!/bin/bash -xe
-cd $HOME/Work/GCC
-if [ -d $HOME/Work/GCC/git ] ; then
-  cd $HOME/Work/GCC/git
+
+# Persistent location for source and binaries
+GCC_HOME=$HOME/GCC
+mkdir -p $GCC_HOME
+
+# Download/update the source
+cd $GCC_HOME
+if [ -d $GCC_HOME/git ] ; then
+  cd $GCC_HOME/git
   git pull
 else
   git clone --depth 10 https://github.com/gcc-mirror/gcc.git git
 fi
+
+# Download the dependencies
+# If wget fails, curl may be better (needs manual edit)
+cd $GCC_HOME/git
+./contrib/download_prerequisites
+
+# Create the temporary build directory in a fast local filesystem
 mkdir -p /tmp/gcc-build
 cd /tmp/gcc-build
-$HOME/Work/GCC/git/configure \
+$GCC_HOME/git/configure \
 --program-suffix=-7 \
 --disable-multilib \
 --enable-threads=posix \
@@ -21,6 +34,6 @@ $HOME/Work/GCC/git/configure \
 --enable-lto \
 --enable-gold=yes \
 --enable-ld=yes \
---prefix=/opt/gcc/master
+--prefix=$GCC_HOME/HEAD
 make -j15
-sudo make install
+make install || sudo make install
