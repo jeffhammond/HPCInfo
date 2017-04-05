b1dc61a14f8d340d87de8454ce0910e805a14502
Jeff Hammond <jeff.science@gmail.com>
2017-04-05 12:17:21 -0700

    sysctl only works on MacOS (or BSD but i have none)

--- linux/sysctl/cpu_model.c
+++ linux/sysctl/cpu_model.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef __APPLE__
 #include <sys/types.h>
 #include <sys/sysctl.h>
+#endif
 
 int main(void)
 {
+#ifdef __APPLE__
     int rc;
     size_t len = 1024;
     char buf[1024] = {0};
     rc = sysctlbyname("machdep.cpu.brand_string", &buf,  &len, NULL, 0);
     printf("len=%zu\n", len);
     printf("buf=%s\n", buf);
+#endif
     return 0;
 }
