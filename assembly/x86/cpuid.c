4a32235c7c5bee049223d9b40725432c29537165
Jeff Hammond <jeff.science@gmail.com>
2017-04-05 11:32:55 -0700

    disable timing code

--- assembly/x86/cpuid.c
+++ assembly/x86/cpuid.c
@@ -2,6 +2,24 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#if 1
+
+int main (void)
+{
+    int32_t a, b, c, d;
+    __cpuid (0 /* vendor string */, a, b, c, d);
+    printf("EAX: %x\nEBX: %x\nECX: %x\nEDX: %x\n", a, b, c, d);
+    a = 1024;
+    for (uint32_t i=1; i<=32; i++) {
+        printf("%d", (a >> i) & 0x00000001 );
+        if ((i%4)==0) printf("|");
+    }
+    printf("\n");
+    return 0;
+}
+
+#else
+
 #include <omp.h>
 
 int main (void)
@@ -19,14 +37,8 @@ int main (void)
     dt = t1-t0;
     printf("cpuid dt=%lf s dt/n=%lf ns\n", dt, 1.e9*(dt/n));
     printf("EAX: %x\nEBX: %x\nECX: %x\nEDX: %x\n", a, b, c, d);
-    a = 1024;
-    for (uint32_t i=1; i<=32; i++) {
-        printf("%d", (a >> i) & 0x00000001 );
-        //printf("\n");
-        //printf("i=%d i mod 4=%d\n", i, i%4);
-        if ((i%4)==0) printf("|");
-    }
-    printf("\n");
     printf("junk=%zu\n", junk);
     return 0;
 }
+
+#endif
