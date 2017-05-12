6110cf8351fa75ba8c9fbe112b5d3de5f558cb2a
Jeff Hammond <jeff.science@gmail.com>
2017-05-12 16:12:16 -0700

    updates and renaming

---
+++ buildscripts/gcc-git.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -xe
+
+MAKE_JNUM="-j32"
+
+# Persistent location for source and binaries
+GCC_HOME=/opt/gcc/
+mkdir -p $GCC_HOME
+
+# Download/update the source
+cd $GCC_HOME
+if [ -d $GCC_HOME/git ] ; then
+  cd $GCC_HOME/git
+  git pull
+else
+  git clone --depth 10 https://github.com/gcc-mirror/gcc.git git
+fi
+
+# Download the dependencies
+# If wget fails, curl may be better (needs manual edit)
+cd $GCC_HOME/git
+./contrib/download_prerequisites
+
+# Create the temporary build directory in a fast local filesystem
+mkdir -p /tmp/gcc-build
+cd /tmp/gcc-build
+$GCC_HOME/git/configure \
+--program-suffix=-8 \
+--disable-multilib \
+--enable-threads=posix \
+--enable-checking=release \
+--with-system-zlib \
+--enable-__cxa_atexit \
+--enable-languages=c,c++,fortran \
+--with-tune=native \
+--enable-bootstrap \
+--enable-lto \
+--enable-gold=yes \
+--enable-ld=yes \
+--prefix=$GCC_HOME/HEAD
+make ${MAKE_JNUM}
+make ${MAKE_JNUM} install || sudo make ${MAKE_JNUM} install
---
+++ buildscripts/gcc-release.sh
@@ -0,0 +1,71 @@
+#!/bin/bash -x
+
+MAKE_JNUM="-j32"
+
+FTP_HOST=ftp://gcc.gnu.org/pub/gcc
+
+GCC_VERSION=5.4.0
+#GCC_VERSION=6.3.0
+#GCC_VERSION=7.1.0
+
+CPU=native
+
+#GCC_DIR=$HOME/Work/GCC/gcc-$GCC_VERSION
+GCC_DIR=/opt/gcc/$GCC_VERSION
+GCC_BUILD=/tmp/gcc-$GCC_VERSION
+
+mkdir -p ${GCC_BUILD}
+
+# process_lib: download, configure, build, install one of the gcc prerequisite
+# libraries
+# usage: process_lib <library> <version> <suffix> <path> <doodad> <configure_args>
+process_lib() {
+    cd ${GCC_BUILD}
+    TOOL=$1
+    TDIR=${TOOL}-${2}
+    FILE=${TDIR}.tar.${3}
+    INSTALLED=${GCC_DIR}/$5
+    if [ -d ${TDIR} ] ; then
+        echo ${TDIR} already exists! Using existing copy.
+    else
+        if [ -f ${FILE} ] ; then
+            echo ${FILE} already exists! Using existing copy.
+        else
+            #wget ${FTP_HOST}/$4/${FILE}
+            curl ${FTP_HOST}/$4/${FILE} -o  ${FILE}
+        fi
+        echo Unpacking ${FILE}
+        tar -xaf ${FILE}
+    fi
+    if [ -f ${INSTALLED} ] ; then
+        echo ${INSTALLED} already exists! Skipping build.
+    else
+        cd ${GCC_BUILD}/${TDIR}
+        if [ -f ./contrib/download_prerequisites ] ; then
+            ./contrib/download_prerequisites
+        fi
+        mkdir build ; cd build
+        ../configure --prefix=${GCC_DIR} $6 && make ${MAKE_JNUM} && make install
+        if [ "x$?" != "x0" ] ; then
+            echo FAILURE 1
+            exit
+        fi
+    fi
+}
+
+process_lib gcc $GCC_VERSION bz2 releases/gcc-$GCC_VERSION /bin/gcc "
+  --program-suffix=-${GCC_VERSION:0:1} \
+  --enable-shared --enable-static \
+  --enable-threads=posix \
+  --enable-checking=release \
+  --with-system-zlib \
+  --enable-__cxa_atexit \
+  --enable-languages=c,c++,fortran \
+  --with-tune=$CPU \
+  --enable-bootstrap \
+  --enable-lto \
+  --enable-gold=yes \
+  --enable-ld=yes \
+  --disable-multilib
+"
+
