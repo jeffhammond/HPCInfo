f737a31225b5b982c0e3f132517ca181bccb0ea4
Jeff Hammond <jeff.r.hammond@intel.com>
2018-01-18 09:42:44 -0800

    reorganize C++ files and add scan

---
+++ c++/scan/scan.cxx
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <numeric> // inclusive_scan
+#include <iterator>
+#include <functional>
+
+#include "tbb/tbb.h"
+
+template <class Container, class String>
+void print(Container & v, String & name)
+{
+    std::cout << name << "\n";
+    for (auto & i : v) {
+        std::cout << i;
+        if (&i == &v.back()) {
+            std::cout << std::endl;
+        } else {
+            std::cout << ", ";
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    size_t n = (argc>1) ? atol(argv[1]) : 1000;
+
+    std::vector<double> in;
+    in.resize(n,0);
+
+    std::vector<double> psum(in);
+    std::vector<double> iscan(in);
+    std::vector<double> xscan(in);
+
+    // initialize input to the sequence of natural numbers
+    std::iota(in.begin(), in.end(), 1);
+    print(in,"in");
+
+    std::partial_sum(in.begin(), in.end(), psum.begin());
+    print(psum,"psum");
+
+#if (defined(__cplusplus) && (__cplusplus >= 201703L))
+    std::inclusive_scan(in.begin(), in.end(), iscan.begin());
+    print(iscan,"iscan");
+
+    std::exclusive_scan(in.begin(), in.end(), xscan.begin());
+    print(xscan,"xscan");
+#endif
+
+    return 0;
+}
