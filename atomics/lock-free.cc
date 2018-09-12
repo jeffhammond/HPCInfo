60e32c84e35238eaee1b66231f84b013d68542f7
Jeff Hammond <jeff.r.hammond@intel.com>
2018-09-12 06:34:04 -0700

    fp atomics not until C++20

--- atomics/lock-free.cc
+++ atomics/lock-free.cc
@@ -4,10 +4,18 @@
 
 int main()
 {
-    std::atomic<long long> all;
-    std::atomic<double> ad;
-    std::atomic< std::complex<double> > acd;
+    std::atomic<long long> all{0};
+    std::atomic<double> ad{0};
+    std::atomic< std::complex<double> > acd{0};
     std::cout << std::atomic_is_lock_free(&all) << "\n";
     std::cout << std::atomic_is_lock_free(&ad) << "\n";
     std::cout << std::atomic_is_lock_free(&acd) << "\n";
+    all = 1;
+    ad  = 1.0;
+    acd = std::complex<double>{1.0,0.0};
+    all.fetch_add(1);
+#if defined(__cplusplus) && !(__cplusplus<=202000L)
+    ad.fetch_add(1.0);
+    acd.fetch_add(std::complex<double>{1.0,0.0});
+#endif
 }
