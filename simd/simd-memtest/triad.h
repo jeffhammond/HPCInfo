094f4335ef2f2f1b50d6860773f4f9c099ea7a55
Hammond, Jeff R <jeff.r.hammond@intel.com>
2019-07-11 15:21:08 -0700

    rename etc

--- simd/simd-memtest/triad.h
+++ simd/simd-memtest/triad.h
@@ -1,40 +1,40 @@
-#ifndef COPY_H
-#define COPY_H
+#ifndef TRIAD_H
+#define TRIAD_H
 
 #include "compiler.h"
 
-void copy_ref(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_mov(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_rep_movsq(size_t n, const double * RESTRICT a, double * RESTRICT b);
+void triad_ref(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_mov(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_rep_movsq(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
 
 /* SSE+ */
-void copy_movnti(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_movnti64(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_movntq64(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_movapd128(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_movntpd128(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_movntdqa128(size_t n, const double * RESTRICT a, double * RESTRICT b);
+void triad_movnti(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_movnti64(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_movntq64(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_movapd128(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_movntpd128(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_movntdqa128(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
 
 /* AVX and AVX-2 */
-void copy_vmovapd256(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_vmovntpd256(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_vmovntdqa256(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_vgatherdpd128(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_vgatherqpd128(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_vgatherdpd256(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_vgatherqpd256(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_mvgatherqpd256(size_t n, const double * RESTRICT a, double * RESTRICT b);
+void triad_vmovapd256(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_vmovntpd256(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_vmovntdqa256(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_vgatherdpd128(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_vgatherqpd128(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_vgatherdpd256(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_vgatherqpd256(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_mvgatherqpd256(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
 
 /* AVX-512F */
-void copy_vmovapd512(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_vmovupd512(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_mvmovapd512(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_mvmovupd512(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_vmovntdqa512(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_vmovntpd512(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_vGSdpd512(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_mvGSdpd512(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_vGSqpd512(size_t n, const double * RESTRICT a, double * RESTRICT b);
-void copy_mvGSqpd512(size_t n, const double * RESTRICT a, double * RESTRICT b);
+void triad_vmovapd512(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_vmovupd512(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_mvmovapd512(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_mvmovupd512(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_vmovntdqa512(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_vmovntpd512(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_vGSdpd512(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_mvGSdpd512(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_vGSqpd512(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
+void triad_mvGSqpd512(size_t n, double s, const double * RESTRICT a, const double * RESTRICT b, double * RESTRICT c);
 
-#endif /* COPY_H */
+#endif /* TRIAD_H */
