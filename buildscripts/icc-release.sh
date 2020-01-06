3f3e797e4ce00323ad4d7af3bef1a92e2562e98f
Jeff Hammond <jeff.r.hammond@intel.com>
2020-01-06 14:26:55 -0800

    add 2020

--- buildscripts/icc-release.sh
+++ buildscripts/icc-release.sh
@@ -9,6 +9,7 @@ mkdir -p $HOME/Downloads
 cd $HOME/Downloads
 BASE=http://registrationcenter-download.intel.com/akdlm/irc_nas/tec
 for x in \
+         16225/parallel_studio_xe_2020_cluster_edition.tgz         \
          15809/parallel_studio_xe_2019_update5_cluster_edition.tgz \
          15533/parallel_studio_xe_2019_update4_cluster_edition.tgz \
          15268/parallel_studio_xe_2019_update3_cluster_edition.tgz \
@@ -45,6 +46,7 @@ for x in \
 done
 
 for x in \
+         parallel_studio_xe_2020_cluster_edition.tgz         \
          parallel_studio_xe_2019_update5_cluster_edition.tgz \
          parallel_studio_xe_2019_update4_cluster_edition.tgz \
          parallel_studio_xe_2019_update3_cluster_edition.tgz \
@@ -81,6 +83,7 @@ for x in \
 done
 
 for x in \
+         parallel_studio_xe_2020_cluster_edition         \
          parallel_studio_xe_2019_update5_cluster_edition \
          parallel_studio_xe_2019_update4_cluster_edition \
          parallel_studio_xe_2019_update3_cluster_edition \
