5bc958f8365f2ca6a87a0c66d3dd4cac8c9fd4e9
Jeff Hammond <jeff.science@gmail.com>
2020-03-30 10:38:39 -0700

    redo Flang

---
+++ buildscripts/flang-new.sh
@@ -0,0 +1,104 @@
+#!/bin/bash -ex
+
+export MAKE_JNUM="make -j1"
+
+# where LLVM source and install will live
+export LLVM_TOP=/opt/llvm/pgi-flang
+
+mkdir -p $LLVM_TOP
+
+WHAT=$LLVM_TOP/llvm-git
+if [ -d $WHAT ] ; then
+    cd $WHAT
+    git fetch --all
+    git checkout release_70
+    git pull
+else
+    cd $LLVM_TOP
+    git clone -b release_70 https://github.com/flang-compiler/llvm.git llvm-git
+fi
+cd $WHAT
+mkdir -p build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$LLVM_TOP \
+         -DLLVM_CONFIG=$INSTALL_PREFIX/bin/llvm-config \
+         -DCMAKE_CXX_COMPILER=$INSTALL_PREFIX/bin/clang++ \
+         -DCMAKE_C_COMPILER=$INSTALL_PREFIX/bin/clang \
+         -DCMAKE_Fortran_COMPILER=$INSTALL_PREFIX/bin/flang \
+         -DLLVM_TARGETS_TO_BUILD=X86 \
+         -DFLANG_OPENMP_GPU_NVIDIA=ON \
+      && $MAKE_JNUM install
+
+
+WHAT=$LLVM_TOP/flang-driver-git
+if [ -d $WHAT ] ; then
+    cd $WHAT
+    git fetch --all
+    git checkout release_70
+    git pull
+else
+    cd $LLVM_TOP
+    git clone -b release_70 https://github.com/flang-compiler/flang-driver.git flang-driver-git
+fi
+cd $WHAT
+mkdir -p build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$LLVM_TOP \
+         -DLLVM_CONFIG=$INSTALL_PREFIX/bin/llvm-config \                              |  -------------------------------------------------------------------------------------
+         -DCMAKE_CXX_COMPILER=$INSTALL_PREFIX/bin/clang++ \                           |  -------------------------------------------------------------------------------------
+         -DCMAKE_C_COMPILER=$INSTALL_PREFIX/bin/clang \                               |  -------------------------------------------------------------------------------------
+         -DCMAKE_Fortran_COMPILER=$INSTALL_PREFIX/bin/flang \                         |  -------------------------------------------------------------------------------------
+         -DLLVM_TARGETS_TO_BUILD=X86 \                                                |  -------------------------------------------------------------------------------------
+         -DFLANG_OPENMP_GPU_NVIDIA=ON \                                               |  -------------------------------------------------------------------------------------
+      && $MAKE_JNUM install
+
+WHAT=$LLVM_TOP/openmp-git
+if [ -d $WHAT ] ; then
+    cd $WHAT
+    git fetch --all
+    git checkout release_70
+    git pull
+else
+    cd $LLVM_TOP
+    git clone -b release_70 https://github.com/llvm-mirror/openmp.git openmp-git
+fi
+cd $WHAT
+mkdir -p build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$LLVM_TOP \
+         -DLLVM_CONFIG=$INSTALL_PREFIX/bin/llvm-config \                              |  -------------------------------------------------------------------------------------
+         -DCMAKE_CXX_COMPILER=$INSTALL_PREFIX/bin/clang++ \                           |  -------------------------------------------------------------------------------------
+         -DCMAKE_C_COMPILER=$INSTALL_PREFIX/bin/clang \                               |  -------------------------------------------------------------------------------------
+         -DCMAKE_Fortran_COMPILER=$INSTALL_PREFIX/bin/flang \                         |  -------------------------------------------------------------------------------------
+         -DLLVM_TARGETS_TO_BUILD=X86 \                                                |  -------------------------------------------------------------------------------------
+         -DFLANG_OPENMP_GPU_NVIDIA=ON \                                               |  -------------------------------------------------------------------------------------
+      && $MAKE_JNUM install
+
+WHAT=$LLVM_TOP/flang-git
+if [ -d $WHAT ] ; then
+    cd $WHAT
+    git fetch --all
+    git pull
+else
+    cd $LLVM_TOP
+    git clone https://github.com/flang-compiler/flang.git flang-git
+fi
+cd $WHAT
+mkdir -p build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$LLVM_TOP \
+         -DLLVM_CONFIG=$INSTALL_PREFIX/bin/llvm-config \                              |  -------------------------------------------------------------------------------------
+         -DCMAKE_CXX_COMPILER=$INSTALL_PREFIX/bin/clang++ \                           |  -------------------------------------------------------------------------------------
+         -DCMAKE_C_COMPILER=$INSTALL_PREFIX/bin/clang \                               |  -------------------------------------------------------------------------------------
+         -DCMAKE_Fortran_COMPILER=$INSTALL_PREFIX/bin/flang \                         |  -------------------------------------------------------------------------------------
+         -DLLVM_TARGETS_TO_BUILD=X86 \                                                |  -------------------------------------------------------------------------------------
+         -DFLANG_OPENMP_GPU_NVIDIA=ON \                                               |  -------------------------------------------------------------------------------------
+      && $MAKE_JNUM install
+
+cd $WHAT/runtime/libpgmath
+mkdir -p build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$LLVM_TOP \
+         -DLLVM_CONFIG=$INSTALL_PREFIX/bin/llvm-config \                              |  -------------------------------------------------------------------------------------
+         -DCMAKE_CXX_COMPILER=$INSTALL_PREFIX/bin/clang++ \                           |  -------------------------------------------------------------------------------------
+         -DCMAKE_C_COMPILER=$INSTALL_PREFIX/bin/clang \                               |  -------------------------------------------------------------------------------------
+         -DCMAKE_Fortran_COMPILER=$INSTALL_PREFIX/bin/flang \                         |  -------------------------------------------------------------------------------------
+         -DLLVM_TARGETS_TO_BUILD=X86 \                                                |  -------------------------------------------------------------------------------------
+         -DFLANG_OPENMP_GPU_NVIDIA=ON \                                               |  -------------------------------------------------------------------------------------
+      && $MAKE_JNUM install
+
