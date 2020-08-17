9b6782ef9ad3abbf5d68173ce51d31219e666f40
Hammond, Jeff R <jeff.r.hammond@intel.com>
2020-08-17 15:52:07 -0700

    does not work on Mac...

---
+++ c++/20/hardware.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <new>
+
+int main(void)
+{
+    std::cout << "std::hardware_destructive_interference_size=" << std::hardware_destructive_interference_size << std::endl;
+    return 0;
+}
