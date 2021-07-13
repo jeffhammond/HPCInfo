68a553976f9501a70943a235fc5ec6f4f6bb600a
Jeff Hammond <jehammond@nvidia.com>
2021-07-13 09:53:29 +0300

    Create setup-arm.sh

---
+++ docs/nwchem/setup-arm.sh
@@ -0,0 +1,43 @@
+module load arm21/21.0
+
+export NWCHEM_TOP=${HOME}/NWCHEM/arm
+export NWCHEM_TARGET=LINUX64
+export NWCHEM_MODULES="all"
+
+#MPI_PATH=/opt/amazon/openmpi
+MPI_PATH=${HOME}/MPI/arm-ompi-4.1.1
+export PATH=${MPI_PATH}/bin/:$PATH
+export LD_LIBRARY_PATH=${MPI_PATH}/lib:$LD_LIBRARY_PATH
+
+export FC=armflang
+export CC=armclang
+export CXX=armclang++
+
+export USE_MPI=T
+export MPI_INCLUDE=${MPI_PATH}/include
+export MPI_LIB=${MPI_PATH}/lib
+#export LIBMPI="-lmpi_usempif08 -lmpi_usempi_ignore_tkr -lmpi_mpifh -lmpi"
+export LIBMPI="-lmpi -lpthread"
+
+#export ARMCI_NETWORK=MPI-PR
+export ARMCI_NETWORK=ARMCI
+export EXTERNAL_ARMCI_PATH=${NWCHEM_TOP}/external-armci
+
+export MPIFC=${MPI_PATH}/bin/mpifort
+export MPIF77=${MPI_PATH}/bin/mpifort
+export MPICC=${MPI_PATH}/bin/mpicc
+export MPICXX=${MPI_PATH}/bin/mpicxx
+export NO_MPIF=y
+
+#export USE_INTERNALBLAS=y
+export BLASOPT="-DOPENBLAS /software/ACFL/21.0/armpl-21.0.0_AArch64_RHEL-7_arm-linux-compiler_aarch64-linux/lib/libarmpl_ilp64.a"
+export BLAS_LIB=${BLASOPT}
+export LAPACK_LIB=${BLASOPT}
+
+#export BUILD_OPENBLAS=y
+
+export USE_OPENMP=y
+export USE_F90_ALLOCATABLE=y
+
+export USE_SIMINT=1 
+export SIMINT_MAXAM=5
