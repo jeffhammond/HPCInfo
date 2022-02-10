ba9987f84c027abbea720f9fd797c4d7625777cc
Jeff Hammond <jeff.science@gmail.com>
2022-02-10 13:55:35 +0200

    i am dumb

---
+++ buildscripts/autotools.sh
@@ -0,0 +1,127 @@
+#!/bin/bash -v
+
+TOP=$HOME/TOOLS
+
+MAKE_JNUM=1
+M4_VERSION=1.4.17
+LIBTOOL_VERSION=2.4.4
+AUTOCONF_VERSION=2.69
+AUTOMAKE_VERSION=1.15
+
+cd ${TOP}
+TOOL=m4
+TDIR=${TOOL}-${M4_VERSION}
+FILE=${TDIR}.tar.gz
+BIN=${TOP}/bin/${TOOL}
+if [ -f ${FILE} ] ; then
+  echo ${FILE} already exists! Using existing copy.
+else
+  wget http://ftp.gnu.org/gnu/${TOOL}/${FILE}
+fi
+if [ -d ${TDIR} ] ; then
+  echo ${TDIR} already exists! Using existing copy.
+else
+  echo Unpacking ${FILE}
+  tar -xzf ${FILE}
+fi
+if [ -f ${BIN} ] ; then
+  echo ${BIN} already exists! Skipping build.
+else
+  cd ${TOP}/${TDIR}                                                                                                                                                   
+  ./configure --prefix=${TOP} && make -j ${MAKE_JNUM} && make install
+  if [ "x$?" != "x0" ] ; then
+    echo FAILURE 1
+    exit
+  fi
+fi
+
+cd ${TOP}
+TOOL=libtool
+TDIR=${TOOL}-${LIBTOOL_VERSION}
+FILE=${TDIR}.tar.gz
+BIN=${TOP}/bin/${TOOL}
+if [ ! -f ${FILE} ] ; then
+  wget http://ftp.gnu.org/gnu/${TOOL}/${FILE}
+else
+  echo ${FILE} already exists! Using existing copy.
+fi
+if [ ! -d ${TDIR} ] ; then
+  echo Unpacking ${FILE}
+  tar -xzf ${FILE}
+else
+  echo ${TDIR} already exists! Using existing copy.
+fi
+if [ -f ${BIN} ] ; then
+  echo ${BIN} already exists! Skipping build.
+else
+  cd ${TOP}/${TDIR}
+  ./configure --prefix=${TOP} M4=${TOP}/bin/m4 && make -j ${MAKE_JNUM} && make install
+  if [ "x$?" != "x0" ] ; then
+    echo FAILURE 2
+    exit
+  fi
+fi
+
+cd ${TOP}
+TOOL=autoconf
+TDIR=${TOOL}-${AUTOCONF_VERSION}
+FILE=${TDIR}.tar.gz
+BIN=${TOP}/bin/${TOOL}
+if [ ! -f ${FILE} ] ; then
+  wget http://ftp.gnu.org/gnu/${TOOL}/${FILE}
+else
+  echo ${FILE} already exists! Using existing copy.
+fi
+if [ ! -d ${TDIR} ] ; then
+  echo Unpacking ${FILE}
+  tar -xzf ${FILE}
+else
+  echo ${TDIR} already exists! Using existing copy.
+fi
+if [ -f ${BIN} ] ; then
+  echo ${BIN} already exists! Skipping build.
+else
+  cd ${TOP}/${TDIR}
+  ./configure --prefix=${TOP} M4=${TOP}/bin/m4 && make -j ${MAKE_JNUM} && make install
+  if [ "x$?" != "x0" ] ; then
+    echo FAILURE 3
+    exit
+  fi
+fi
+
+cd ${TOP}
+TOOL=automake
+TDIR=${TOOL}-${AUTOMAKE_VERSION}
+FILE=${TDIR}.tar.gz
+BIN=${TOP}/bin/${TOOL}
+if [ ! -f ${FILE} ] ; then
+  wget http://ftp.gnu.org/gnu/${TOOL}/${FILE}
+else
+  echo ${FILE} already exists! Using existing copy.
+fi
+if [ ! -d ${TDIR} ] ; then
+  echo Unpacking ${FILE}
+  tar -xzf ${FILE}
+else
+  echo ${TDIR} already exists! Using existing copy.
+fi
+if [ -f ${BIN} ] ; then
+  echo ${BIN} already exists! Skipping build.
+else
+  cd ${TOP}/${TDIR}
+  ./configure --prefix=${TOP} M4=${TOP}/bin/m4 && make -j ${MAKE_JNUM} && make install
+  if [ "x$?" != "x0" ] ; then
+    echo FAILURE 4
+    exit
+  fi
+fi
+
+cd ${TOP}
+#rm -f autoconf-${AUTOCONF_VERSION}.tar.gz
+#rm -f automake-${AUTOMAKE_VERSION}.tar.gz
+#rm -f libtool-${LIBTOOL_VERSION}.tar.gz
+#rm -f m4-${M4_VERSION}.tar.gz
+#rm -rf autoconf-${AUTOCONF_VERSION}
+#rm -rf automake-${AUTOMAKE_VERSION}
+#rm -rf libtool-${LIBTOOL_VERSION}
+#rm -rf m4-${M4_VERSION}
