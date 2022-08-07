a5c2506d65025da570ff835d607beef4b64485f2
Jeff Hammond <jeff.science@gmail.com>
2022-08-07 17:37:29 +0300

    working as best i can tell

--- fortran/pgif/foo_cfi.c
+++ fortran/pgif/foo_cfi.c
@@ -51,14 +51,32 @@ typedef struct F90_Desc_la {
   struct F90_DescDim_la dim[MAXDIMS];
 } F90_Desc_la;
 
-void foo(int * buffer, int * m, int * n)
+void foo(int * buffer, int * m, int * n, int * o)
 {
     printf("FOO buffer = %p\n", buffer);
     printf("FOO m,n = %d,%d\n", *m, *n);
 }
 
-void bar(int * buffer, int * m, int * n)
+void bar(int * buffer, int * m, int * n, int * o, F90_Desc_la * d)
 {
     printf("BAR buffer = %p\n", buffer);
-    printf("BAR m,n = %d,%d\n", *m, *n);
+    printf("BAR m,n,o = %d,%d,%d\n", *m, *n, *o);
+    printf("BAR F90_Desc = %p\n", d);
+    printf("BAR F90_Desc->tag   = %lld\n", d->tag  );
+    printf("BAR F90_Desc->rank  = %lld\n", d->rank );
+    printf("BAR F90_Desc->kind  = %lld\n", d->kind );
+    printf("BAR F90_Desc->len   = %lld\n", d->len  );
+    printf("BAR F90_Desc->flags = %lld\n", d->flags);
+    printf("BAR F90_Desc->lsize = %lld\n", d->lsize);
+    printf("BAR F90_Desc->gsize = %lld\n", d->gsize);
+    printf("BAR F90_Desc->lbase = %lld\n", d->lbase);
+    printf("BAR F90_Desc->gbase = %p\n", d->gbase);
+    for (int i=0; i<d->rank; i++) {
+        printf("BAR F90_Desc->dim.lbound  = %lld\n", d->dim[i].lbound );
+        printf("BAR F90_Desc->dim.extent  = %lld\n", d->dim[i].extent );
+        printf("BAR F90_Desc->dim.sstride = %lld\n", d->dim[i].sstride);
+        printf("BAR F90_Desc->dim.soffset = %lld\n", d->dim[i].soffset);
+        printf("BAR F90_Desc->dim.lstride = %lld\n", d->dim[i].lstride);
+        printf("BAR F90_Desc->dim.ubound  = %lld\n", d->dim[i].ubound );
+    }
 }
--- fortran/pgif/test_cfi.F90
+++ fortran/pgif/test_cfi.F90
@@ -1,17 +1,17 @@
 module cfi
     interface
-        subroutine foo(buffer,m,n) bind(C,name="foo")
+        subroutine foo(buffer,m,n,o) bind(C,name="foo")
             implicit none
             integer, dimension(*) :: buffer
-            integer :: m, n
+            integer :: m, n, o
         end subroutine foo
     end interface
 
     interface
-        subroutine bar(buffer,m,n) bind(C,name="bar")
+        subroutine bar(buffer,m,n, o) bind(C,name="bar")
             implicit none
             integer, dimension(..) :: buffer
-            integer :: m, n
+            integer :: m, n, o
         end subroutine bar
     end interface
 end module cfi 
@@ -20,19 +20,21 @@ program test
     use iso_c_binding
     use cfi
     implicit none
-    integer, dimension(10,10) :: a
-    integer :: i, j
+    integer, dimension(5,7,9) :: a
+    integer :: i, j, k
 
-    do i=1,10
-      do j=1,10
-        a(j,i) = 100*j + 100*100*i
+    do i=1,size(a,3)
+      do j=1,size(a,2)
+        do k=1,size(a,1)
+          a(k,j,i) = 10*k + 10*100*j + 10*100*100*i
+        end do
       end do
     end do
 
-    call foo(a,size(a,1),size(a,2))
+    call foo(a,size(a,1),size(a,2),size(a,3))
     !call foo(a(1,1),size(a,1),size(a,2))
 
-    call bar(a,size(a,1),size(a,2))
+    call bar(a,size(a,1),size(a,2),size(a,3))
     !call bar(a(1,1),size(a,1),size(a,2))
 
     !call foo(a(:,:))
