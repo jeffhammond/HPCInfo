66420c5a5fa79915f125d922143ef368997d66d1
Jeff Hammond <jeff.science@gmail.com>
2022-08-26 13:26:32 +0300

    beginning

--- fortran/pgif/foo_pgif.F90
+++ fortran/pgif/foo_pgif.F90
@@ -4,16 +4,30 @@ module pgif
 
     contains
 
-        subroutine foo(buffer,m,n) bind(C,name="foo")
+        subroutine foo(b,m,n) bind(C,name="foo")
             implicit none
-            real(kind=REAL64), dimension(*) :: buffer
-            integer :: m, n
+            real(kind=REAL64), dimension(m,n) :: b
+            integer, value :: m, n
+            integer :: i, j
+            print*,'foo',m
+            print*,'foo',n
+            !print*,'foo',b(1:m,1:n)
+            do j=1,n
+              do i=1,m
+                print*,'foo',i,j,b(i,j)
+              end do
+            end do
         end subroutine foo
 
-        subroutine bar(buffer,m,n) bind(C,name="bar")
+        subroutine bar(b,m,n) bind(C,name="bar")
             implicit none
-            real(kind=REAL64), dimension(..) :: buffer
-            integer :: m, n
+            real(kind=REAL64), dimension(..) :: b
+            integer, value :: m, n
+            print*,'bar',m
+            print*,'bar',n
+            !print*,'bar',size(b,1)
+            !print*,'bar',size(b,2)
+            !print*,'bar',b
         end subroutine bar
 
 end module pgif
--- fortran/pgif/test_pgif.c
+++ fortran/pgif/test_pgif.c
@@ -3,11 +3,33 @@
 
 #include "pgif90.h"
 
+#define M 3
+#define N 4
+
+extern void foo(double *, int, int);
+extern void bar(double *, int, int, F90_Desc *);
+
 int main(void)
 {
+    double x[M][N];
+    double * y = malloc(M*N*sizeof(*y));
 
+    int z = 0;
+    for (int i=0; i<M; i++) {
+        for (int j=0; j<N; j++) {
+            x[i][j] = y[i*N+j] = z++;
+        }
+    }
 
+    for (int i=0; i<M; i++) {
+        for (int j=0; j<N; j++) {
+            printf("C: %d,%d,%f,%f\n",i,j,x[i][j],y[i*N+j]);
+        }
+    }
 
+    foo(y,N,M);
+    bar(y,N,M,NULL);
 
-
+    free(y);
+    return 0;
 }
