4dc7291a50bc4263f4b5cbb242234951a00bef45
Jeff Hammond <jeff.science@gmail.com>
2022-08-27 14:00:48 +0300

    should keep the license

--- fortran/pgif/pgif90.h
+++ fortran/pgif/pgif90.h
@@ -1,15 +1,17 @@
+/*
+ *     Copyright (c) 2017-2018, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * NVIDIA CORPORATION and its licensors retain all intellectual property
+ * and proprietary rights in and to this software, related documentation
+ * and any modifications thereto.  Any use, reproduction, disclosure or
+ * distribution of this software and related documentation without an express
+ * license agreement from NVIDIA CORPORATION is strictly prohibited.
+ *
+ */
+
 // from /opt/nvidia/hpc_sdk/Linux_x86_64/22.7/compilers/include_acc/nvhpc_cuda_runtime.h
 
 #define MAXDIMS 7
-
-struct F90_DescDim {
-  int lbound;
-  int extent;
-  int sstride;
-  int soffset;
-  int lstride;
-  int ubound;
-};
 
 struct F90_DescDim_la {
   long long lbound;
@@ -19,20 +21,6 @@ struct F90_DescDim_la {
   long long lstride;
   long long ubound;
 };
-
-typedef struct F90_Desc {
-  int tag;
-  int rank;
-  int kind;
-  int len;
-  int flags;
-  int lsize;
-  int gsize;
-  int lbase;
-  int *gbase;
-  int *unused;
-  struct F90_DescDim dim[MAXDIMS];
-} F90_Desc;
 
 typedef struct F90_Desc_la {
   long long tag;
