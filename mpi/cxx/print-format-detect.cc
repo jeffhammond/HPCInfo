610e5463ac0d62f1820b9d6a45a57216f855fd49
Jeff Hammond <jeff.science@gmail.com>
2023-01-02 09:41:53 +0200

    for Jim

--- mpi/cxx/print-format-detect.cc
+++ mpi/cxx/print-format-detect.cc
@@ -1,18 +1,23 @@
 #include <typeinfo>
 #include <iostream>
+#include <string>
 
 #include <mpi.h>
 
 int main(void)
 {
-    MPI_Count  c = 0;
-    MPI_Aint   a = 0;
-    MPI_Offset o = 0;
-    std::cout << "MPI_Count  is " << typeid(MPI_Count).name() << std::endl;
-    std::cout << "MPI_Aint   is " << typeid(MPI_Aint).name() << std::endl;
-    std::cout << "MPI_Offset is " << typeid(MPI_Offset).name() << std::endl;
-    std::cout << "MPI_Count  is " << typeid(c).name() << std::endl;
-    std::cout << "MPI_Aint   is " << typeid(a).name() << std::endl;
-    std::cout << "MPI_Offset is " << typeid(o).name() << std::endl;
+    MPI_Count  c = 5;
+    MPI_Aint   a = 6;
+    MPI_Offset o = 7;
+
+    std::string ff{"C=%"+std::string{typeid(MPI_Count).name()}+"\n"};
+    printf(ff.c_str(),c);
+
+    std::string gg{"A=%"+std::string{typeid(MPI_Aint).name()}+( std::is_signed<MPI_Aint>() ? "d" : "u")+"\n"};
+    printf(gg.c_str(),a);
+
+    std::string hh{"O=%"+std::string{typeid(MPI_Offset).name()}+"\n"};
+    printf(hh.c_str(),o);
+
     return 0;
 }
