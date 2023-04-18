58b33da084f831c16bbc6f73d48e5d2b001c400e
Jeff Hammond <jeff.science@gmail.com>
2023-04-18 17:28:54 +0300

    F node

--- mpi/mpmd/fbin.F90
+++ mpi/mpmd/fbin.F90
@@ -16,10 +16,35 @@ program main
     use mpi_f08
     use affinity
     implicit none
-    integer :: required, provided
-    required = MPI_THREAD_SERIALIZED
-    call MPI_Init_thread(required,provided)
-    if (provided.lt.required) stop
+    integer :: me, np
+
+    block
+        integer :: required, provided
+        required = MPI_THREAD_SERIALIZED
+        call MPI_Init_thread(required,provided)
+        if (provided.lt.required) stop
+    end block
+
+    block
+        call MPI_Comm_rank(MPI_COMM_WORLD, me)
+        call MPI_Comm_size(MPI_COMM_WORLD, np)
+        print*,'I am ',me,' of ',np
+    end block
+
+    flush(0)
+    call MPI_Barrier(MPI_COMM_WORLD)
+
+    block
+        type(MPI_Comm) :: node
+        integer :: node_me, node_np
+        call MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, node)
+        call MPI_Comm_rank(node, node_me)
+        call MPI_Comm_size(node, node_np)
+        print*,'rank ',me,' is the ',node_me,' rank of ',node_np
+    end block
+
+    flush(0)
+    call MPI_Barrier(MPI_COMM_WORLD)
 
     call MPI_Finalize
 end program main
