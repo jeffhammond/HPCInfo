2c0c44aa909b476abf44f3eef674f1dab92505bd
Jeff Hammond <jeff.science@gmail.com>
2023-07-12 08:23:04 +0300

    test of OACC semantics

---
+++ openacc/nest.F90
@@ -0,0 +1,30 @@
+module m
+
+contains
+
+pure subroutine inner(x,y)
+    implicit none
+    real, intent(in) :: x(:)
+    real, intent(out) :: y(:)
+    integer :: i, n
+    n = max(size(x),size(y))
+!$acc parallel loop
+    do concurrent (i=1:n)
+        y(i) = x(i)
+    end do
+end subroutine inner
+
+end module m
+
+subroutine outer(x,y)
+    use m
+    implicit none
+    real, intent(in) :: x(:,:)
+    real, intent(out) :: y(:,:)
+    integer :: i, n
+    n = max(size(x,2),size(y,2))
+!$acc parallel loop
+    do concurrent (i=1:n)
+        call inner(x(:,i),y(:,i))
+    end do
+end subroutine outer
