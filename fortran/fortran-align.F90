1f340b21d1e624202886c52fc6ec105f8367c9b7
Jeff Hammond <jeff.science@gmail.com>
2023-11-15 10:16:33 +0200

    it does not work

--- fortran/fortran-align.F90
+++ fortran/fortran-align.F90
@@ -9,13 +9,18 @@ program main
   real(kind=REAL64), pointer :: B(:,:)
 
   interface
+    ! void *malloc(size_t size);
+    type(c_ptr) function malloc(size) bind(C,name="malloc")
+      import c_ptr, c_size_t
+      implicit none
+      integer(kind=c_size_t) :: size
+    end function malloc    
     ! void *aligned_alloc(size_t alignment, size_t size);
     type(c_ptr) function aligned_alloc(alignment, size) bind(C,name="aligned_alloc")
       import c_ptr, c_size_t
       implicit none
       integer(kind=c_size_t) :: alignment, size
     end function aligned_alloc    
-#if 0
     ! int posix_memalign(void **memptr, size_t alignment, size_t size);
     integer(c_int) function posix_memalign(memptr, alignment, size) bind(C,name="posix_memalign")
       import c_ptr, c_size_t, c_int
@@ -23,12 +28,14 @@ program main
       type(c_ptr) :: memptr
       integer(kind=c_size_t) :: alignment, size
     end function posix_memalign
-#endif
   end interface
 
   bytes = n * n * 8 ! lazy: sizeof(REAL64) = 8
-  P = aligned_alloc(alignment = page_size, size = bytes)
+  !P = aligned_alloc(alignment = page_size, size = bytes)
+  P = malloc(size = bytes)
   call c_f_pointer(P,B,[n,n])
+
+  B = 0
                         
   block
     integer :: i,j
