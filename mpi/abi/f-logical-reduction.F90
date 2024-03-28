8d9fc0b23ff2f994b89a841153e6e17e7b77449f
Jeff Hammond <jeff.science@gmail.com>
2024-03-28 10:00:54 +0200

    more
    Signed-off-by: Jeff Hammond <jeff.science@gmail.com>

---
+++ mpi/abi/f-logical-reduction.F90
@@ -0,0 +1,68 @@
+subroutine user_logical_and(invec, inoutvec, len, datatype)
+    use, intrinsic :: iso_c_binding, only : c_ptr
+    use mpi_f08
+    implicit none
+    type(c_ptr), value, intent(in) :: invec, inoutvec
+    integer, intent(in) :: len
+    type(MPI_Datatype), intent(in) :: datatype
+    logical, dimension(:), pointer :: fpi, fpo
+    character(len=MPI_MAX_OBJECT_NAME) :: name
+    integer :: n
+    if (datatype .ne. MPI_LOGICAL) then
+        call MPI_Type_get_name(datatype, name, n)
+        print*,'datatype (',name,') does not match arguments!'
+        call MPI_Abort(MPI_COMM_WORLD,1)
+    else
+        call c_f_pointer(invec,fpi,[len])
+        call c_f_pointer(inoutvec,fpo,[len])
+    end if
+    fpo = fpo .and. fpi
+end subroutine user_logical_and
+
+program main
+    use mpi_f08
+    implicit none
+    integer :: i, me, np, n, argc, arglen, argerr
+    integer, allocatable, dimension(:) :: a, b
+    character(len=64) :: argtmp
+    type(MPI_Op) :: op
+    procedure(MPI_User_function), pointer :: fp => NULL()
+
+    call MPI_Init()
+
+    argc = command_argument_count()
+    if (argc.ge.1) then
+        call get_command_argument(1,argtmp,arglen,argerr)
+        read(argtmp,'(i10)') n
+    else
+        n = 1000
+    endif
+
+    call MPI_Comm_rank(MPI_COMM_WORLD, me)
+    call MPI_Comm_size(MPI_COMM_WORLD, np)
+
+    call MPI_Op_create(fp, .true., op)
+
+    allocate( a(n), b(n) )
+
+    do i=1,n
+        a(i) = merge(1,0,np .gt. 1)
+    end do
+
+    call MPI_Allreduce(a, b, n, MPI_INT, op, MPI_COMM_WORLD)
+
+    do i=1,n
+        if (b(i) .ne. merge(1,0,np .gt. 1)) then
+            call MPI_Abort(MPI_COMM_SELF,np)
+        end if
+    end do
+
+    deallocate( a, b )
+
+    call MPI_Op_free(op)
+
+    if (me.eq.0) print*,'OK'
+
+    call MPI_Finalize()
+
+end program main
