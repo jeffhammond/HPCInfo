a6ace065f11a857003578c2b2fb44af13f15e2fe
Jeff Hammond <jeff.science@gmail.com>
2024-04-03 01:58:56 -0700

    more

--- mpi/abi/fsizes.F90
+++ mpi/abi/fsizes.F90
@@ -1,5 +1,15 @@
 program main
     use iso_fortran_env
     implicit none
+    logical :: l
+    integer :: i
+    real :: r
+    double precision :: d
+    complex :: c
     print*,'NUMERIC_STORAGE_SIZE=',NUMERIC_STORAGE_SIZE
+    print*,'STORAGE_SIZE(LOGICAL)=',STORAGE_SIZE(l)
+    print*,'STORAGE_SIZE(INTEGER)=',STORAGE_SIZE(i)
+    print*,'STORAGE_SIZE(REAL)=',STORAGE_SIZE(r)
+    print*,'STORAGE_SIZE(DOUBLE PRECISION)=',STORAGE_SIZE(d)
+    print*,'STORAGE_SIZE(COMPLEX)=',STORAGE_SIZE(c)
 end program main 
