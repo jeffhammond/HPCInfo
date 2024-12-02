46a090f391eda03b1e2ec0d0aa100197567fa73b
Jeff Hammond <jeff.science@gmail.com>
2024-12-02 10:21:45 +0200

    mac still does not work

--- fortran/cfi/mmdspan.cc
+++ fortran/cfi/mmdspan.cc
@@ -1,12 +1,27 @@
 #include <iostream>
+#include <print>
 #include <mdspan>
 #include "ISO_Fortran_binding.h"
 
+template<typename T>
+void mdspan_typed(CFI_cdesc_t * d)
+{
+    auto mds = std::mdspan(d->base_addr);
+}
+
 extern "C" {
 
-void mdspan(CFI_cdesc_t * d)
-{
-
-}
+    void mdspan(CFI_cdesc_t * d) {
+        switch (d->type)
+        {
+            case CFI_type_float:
+                mdspan_typed<float>(d);
+                break;
+            default:
+                printf("Unknown type\n");
+                abort();
+                break;
+        }
+    }
 
 }
