efae7feeebfca24a4a421d718737d0af7230e718
Jeff Hammond <jeff.science@gmail.com>
2024-12-02 12:39:57 +0200

    works for contiguous only

--- fortran/cfi/mmdspan.cc
+++ fortran/cfi/mmdspan.cc
@@ -2,13 +2,28 @@
 #include <experimental/mdspan>
 #include "ISO_Fortran_binding.h"
 
+using namespace std;
 using namespace std::experimental;
 
 template<typename T>
 void mdspan_typed(CFI_cdesc_t * d)
 {
     std::cout << "mdspan_typed" << std::endl;
-    auto mds = mdspan(static_cast<T*>(d->base_addr), layout_left);
+    auto mds = mdspan<T,
+                      std::extents<CFI_index_t, dynamic_extent>,
+                      layout_left>
+                     (static_cast<T*>(d->base_addr),
+                      dextents<CFI_index_t,1>
+                      {d->dim[0].extent});
+
+    std::cout << "rank() = " << mds.rank() << "\n";
+    std::cout << "size() = " << mds.size() << "\n";
+
+    for (int i = 0; i < mds.rank(); i++)
+        std::cout << "extent(" << i << ") = " << mds.extent(i) << "\n";
+
+    for (size_t i = 0; i < mds.extent(0); i++)
+        std::cout << i << "," << mds(i) << "\n";
 }
 
 extern "C" {
